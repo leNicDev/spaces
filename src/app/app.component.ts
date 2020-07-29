da10bb7e3d0e1ae145e2227b62e61825e8ed73a3
Niclas Stelzer <niclas.stelzer@itsd-consulting.de>
2020-07-29 21:41:14 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'word-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'word';
+}
